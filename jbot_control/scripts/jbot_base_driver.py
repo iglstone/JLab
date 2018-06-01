0ac0013017ef610806a0fd5d1e2ba068754dfdc2
guolong <i690193240@icloud.com>
2018-06-01 16:22:35 +0800

    format

--- jbot_control/scripts/jbot_base_driver.py
+++ jbot_control/scripts/jbot_base_driver.py
@@ -118,7 +118,7 @@ class JBotBaseDriver(object):
                     vx = float(rcv_list[1]) / 1000 * scal_y
                     vz = float(rcv_list[2]) / 1000 * scal_th
 
-		        '''
+                    '''
                     # if new one much bigger than old one, not update
                     if abs(vx - vx_1) > thresh_hold:  # or abs(vy_1 - vy) > thresh_hold or abs(vz_1 - vz) > thresh_hold:
                         # vx = vx_1
@@ -131,7 +131,7 @@ class JBotBaseDriver(object):
                         vx_1 = vx  # update old one
                         vy_1 = vy
                         vz_1 = vz
-		        '''
+                    '''
 
                     slide_height = 0.5  # float(rcv_list[3])
                     if vx != 0 or vy != 0 or vz != 0:
